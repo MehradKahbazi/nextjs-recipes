63202e9cd3fb306e80ef297df0978dc81022a201
unknown <mehradhetfield@gmail.com>
2025-03-31 06:25:19 +0330

    feat: nav, header, card, design

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,16 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
-
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 
 interface Recipe {
   title: string,
@@ -13,8 +22,8 @@ interface Recipe {
   vegan: boolean,
   id: string
 }
-async function getData (): Promise<Recipe[]>{
-  const data =[
+async function getData(): Promise<Recipe[]> {
+  const data = [
     {
       id: "1",
       title: "Veggie Carbonara",
@@ -37,14 +46,6 @@ async function getData (): Promise<Recipe[]>{
       image: "/veg_alfredo.jpg",
       time: 30,
       description: "Creamy pasta with grilled vegetarian chicken and a rich Alfredo sauce.",
-      vegan: false
-    },
-    {
-      id: "4",
-      title: "Mushroom Risotto",
-      image: "/mushroom_risotto.jpg",
-      time: 40,
-      description: "A comforting vegetarian Italian rice dish with sautéed mushrooms and parmesan cheese.",
       vegan: false
     },
     {
@@ -79,14 +80,6 @@ async function getData (): Promise<Recipe[]>{
       description: "Tasty vegetarian tacos with seasoned plant-based meat and fresh toppings.",
       vegan: true
     },
-    {
-      id: "9",
-      title: "Chocolate Cookies",
-      image: "/chocolate_cookies.jpg",
-      time: 12,
-      description: "Homemade vegetarian chocolate chip cookies that are soft and gooey inside.",
-      vegan: false
-    }
   ]
 
   await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -96,36 +89,77 @@ async function getData (): Promise<Recipe[]>{
 export default async function Home() {
   const data = await getData()
   return (
-    <main>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mx-auto">
-        {data.map(item => (
-          <Card key={item.id} className="flex flex-col justify-between mx-4">
-            <CardHeader className="flex flex-row gap-4 items-center">
-              <Avatar>
-                <AvatarImage src={`/img/${item.image}`} alt="recipe image" />
-                <AvatarFallback>
-                  {item.title.slice(0,2)}
-                </AvatarFallback>
-              </Avatar>
-              <div>
-                <CardTitle>{item.title}</CardTitle>
-                <CardDescription>{item.time} mins to cook</CardDescription>
-              </div>
-            </CardHeader>
-            <CardContent>
-            <Image src={`/img${item.image}`} width={300} height={300} alt=""/>
+    <>
+      <NavigationMenu className="h-[7vh] fixed z-20 !w-screen bg-[#2c3f5bd9]">
+        <NavigationMenuList className="w-[30vw] justify-around">
+          <NavigationMenuItem className="w-1/4 text-center text-white">
+            <NavigationMenuLink className="hover:bg-transparent hover:border-b hover:text-white rounded-none">Link</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="w-1/4 text-center text-white">
+            <NavigationMenuLink className="hover:bg-transparent hover:border-b hover:text-white rounded-none">Link</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="w-1/4 text-center text-white">
+            <NavigationMenuLink className="hover:bg-transparent hover:border-b hover:text-white rounded-none">Link</NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <header className="relative">
+        <Image className=" w-full h-auto" src="/img/close-up-chocolate-dessert-with-sugar-powder.jpg" width={2000} height={1500} alt="" />
+        <div className=" static lg:absolute z-10 text-white top-1/4 left-1/12  p-9 rounded-lg w-full lg:w-1/3">
+          <h1 className="mb-3">My Recipe Website</h1>
+          <small >Here I share recipes I enjoy</small>
+          <div className="mt-5">
+            <h3 className="font-bold">
+              About Me
+            </h3>
+            <p>
+              I'm a web developer with a significant passion for food and cakes
+              I'm a web developer with a significant passion for food and cakes
+              I'm a web developer with a significant passion for food and cakes
+              I'm a web developer with a significant passion for food and cakes
+            </p>
+          </div>
+        </div>
+      </header>
+      <main className=" static top-0 lg:relative lg:-top-40">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mx-auto">
+          {data.map(item => (
+            <Card key={item.id} className="flex flex-col justify-between mx-4 pt-0">
+              <Image className="w-full rounded-xl rounded-b-none" src={`/img${item.image}`} width={300} height={300} alt="" />
+              <CardHeader className="flex flex-row gap-4 items-start justify-between">
 
-              <p>
-                {item.description}
-              </p>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button >View recipe</Button>
-              {item.vegan && <Badge variant="secondary">Vegan!</Badge>}
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
-    </main>
+                <div>
+                  <CardTitle>{item.title}</CardTitle>
+                  <CardDescription>{item.time} mins to cook</CardDescription>
+                </div>
+                <div>
+                  {item.vegan && <Badge variant="secondary">Vegan!</Badge>}
+
+                </div>
+              </CardHeader>
+              <CardContent>
+
+                <p>
+                  {item.description}
+                </p>
+              </CardContent>
+              <CardFooter className="flex items-start justify-between">
+                <Avatar>
+                  <AvatarImage src={`/img/${item.image}`} alt="recipe image" />
+                  <AvatarFallback>
+                    {item.title.slice(0, 2)}
+                  </AvatarFallback>
+                </Avatar>
+                <div>
+                  <h6>Mehrad Kahbazi</h6>
+                  <small className="text-slate-500">2 Hours Ago</small>
+                </div>
+                <Button>View recipe</Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
